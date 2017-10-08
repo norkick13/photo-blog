9652213e18554112ac3419bbbdbd4f6f159b040a
Norvinder Singh <norvinder@gmail.com>
2017-10-08 17:31:26 -0400

    first commit

---
+++ src/app/entries/entry/entry-comment-form.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+    selector: 'app-entry-comment-form',
+    templateUrl: 'entry-comment-form.component.html',
+
+})
+
+export class EntryCommentFormComponent {
+    name: string = "";
+    comment:  string = "";
+    @Output() onCommentAdded = new EventEmitter <{name:string; comment:string;}>();
+    @ViewChild('commentForm') commentForm: NgForm;
+
+    onsubmit(commentForm: NgForm ) {
+        let comment = { name: this.name, comment: this.comment };
+        this.onCommentAdded.emit(comment);
+        this.commentForm.resetForm();
+    }
+}
